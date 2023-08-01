8324f5ffdd49e8867f50cbb075893fe703bf32f3
bina <beeina@hotmail.com>
2023-07-31 18:50:59 -0700

    index.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
+cars = [
+   {'brand': 'Honda',
+    'color': 'metalic grey',
+    'model': 'CR-V',
+    'year': '2020',
+    'countryItMade': 'Japan'
+    },
+    {'brand': 'Tesla',
+     'color': 'white',
+     'model': 'Y',
+     'year': '2021',
+     'countryItMade': 'USA'
+     },
+     {'brand': 'Lexus',
+      'color': 'black',
+      'model': 'RC F',
+      'year': '2022',
+      'countryItMade': 'USA'}
+]
 
 # Create your views here.
 def home(request):
@@ -7,3 +26,8 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def cars_index(request):
+  return render(request, 'cars/index.html', {
+    'cars': cars
+  })
