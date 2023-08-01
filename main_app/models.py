753633130233ad61cb81c6c0e8df5ed2510bf5ba
bina <beeina@hotmail.com>
2023-08-01 16:03:50 -0700

    create model

--- main_app/models.py
+++ main_app/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    features = models.CharField(max_length=100)
+    year = models.IntegerField()
